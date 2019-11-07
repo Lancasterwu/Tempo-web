dd231c5bd6c0305781ce1b27c7a9d7dcfb504bdc
Lancaster Wu <wuj@allegheny.edu>
2019-11-07 13:04:59 -0500

    initial

---
+++ src/views/index-sections/Download.js
@@ -0,0 +1,135 @@
+import React from "react";
+
+// reactstrap components
+import { Button, Container, Row, Col, UncontrolledTooltip } from "reactstrap";
+
+// core components
+
+function Download() {
+  return (
+    <>
+      <div
+        className="section section-download"
+        style={{
+          backgroundImage: "url(" + require("assets/img/webpage2.jpg") + ")",
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+          minHeight: "700px"
+        }}
+        id="download-section"
+      >
+        <Container>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Row className="text-center mt-5">
+            <Col className="ml-auto mr-auto" md="8">
+              <h2>Home</h2>
+              <h5 className="description">
+              Galaxy/Moon As the Background
+              Statement: The edge of the world is the edge
+              of imagination. I believed it, and now, I want to
+              mapping the world with the action.
+              Side Face can be a drawing instead of an actual
+              photo.
+              </h5>
+            </Col>
+          </Row>
+          <br></br>
+          <br></br>
+          <Row className="justify-content-md-center sharing-area text-center">
+            <Col className="text-center" lg="8" md="12">
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="twitter"
+                href="https://www.twitter.com/creativetim?ref=creativetim"
+                id="tooltip86114138"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-twitter"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip86114138">
+                Follow us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="facebook"
+                href="https://www.facebook.com/creativetim?ref=creativetim"
+                id="tooltip735272548"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-square"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip735272548">
+                Like us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="linkedin"
+                href="https://www.linkedin.com/company-beta/9430489/?ref=creativetim"
+                id="tooltip647117716"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip647117716">
+                Follow us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-neutral btn-icon btn-round"
+                color="github"
+                href="https://github.com/creativetimofficial/now-ui-kit-react?ref=creativetim"
+                id="tooltip331904895"
+                size="lg"
+                target="_blank"
+              >
+                <i className="fab fa-github"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip331904895">
+                Star on Github
+              </UncontrolledTooltip>
+            </Col>
+          </Row>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Download;
---
+++ src/views/index-sections/Download2.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+// reactstrap components
+import { Button, Container, Row, Col, UncontrolledTooltip } from "reactstrap";
+
+// core components
+
+function Download2() {
+  return (
+    <>
+      <div
+        className="section section-download"
+        style={{
+          backgroundImage: "url(" + require("assets/img/webpage3.jpg") + ")",
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+          minHeight: "700px"
+        }}
+        id="download-section"
+      >
+        <Container>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Row className="text-center mt-5">
+            <Col className="ml-auto mr-auto" md="8">
+              <h2>About</h2>
+              <h5 className="description">
+              Sunset Moment with Clouds as the Background
+              Introduction: Creator, Scientist's Spirit
+              </h5>
+            </Col>
+          </Row>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Download2;
